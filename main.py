056930519512a8ca211770eb34d2a9b6cf5b3502
Kenny Lam <hk_lam@live.com>
2018-04-05 21:08:19 +0200

    Added libraries

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import RPi_I2C_driver
 import MFRC522
 import signal
+from time import *
 
 continue_reading = True
 
@@ -10,6 +11,14 @@ def end_read(signal,frame):
     print "Ctrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
+    pass
+
+def get_time():
+    mylcd.lcd_display_string("Havenlab", 1)
+    mylcd.lcd_display_string("Time remaining", 2)
+    mylcd.lcd_display_string("Time: %s" % time.strftime("%H:%M:%S"), 1)
+    pass
+
 
 signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
@@ -17,17 +26,19 @@ MIFAREReader = MFRC522.MFRC522()
 # Welcome message
 print "Starting MFRC522 data reader"
 print "Press Ctrl-C to stop."
-mylcd = RPi_I2C_driver.lcd()
-mylcd.lcd_display_string("Havenlab", 1)
-mylcd.lcd_display_string("Time remaining", 2)
 
+mylcd = RPi_I2C_driver.lcd()
 
 while continue_reading:
-    
+
+    get_time()
+
     # Scan for cards    
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         mylcd.lcd_clear()
-        mylcd.lcd_display_string("Reservation made")
+        mylcd.lcd_display_string("Reservation has", 1)
+        mylcd.lcd_display_string("been made!", 2)
+        sleep(5)
