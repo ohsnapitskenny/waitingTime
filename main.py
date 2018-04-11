3f21cafddcb202c2514d63aad3e48f06f557881b
Kenny Lam <hk_lam@live.com>
2018-04-11 22:46:35 +0200

    Changed text

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ import signal
 from time import *
 
 continue_reading = True
-countdownTimer = 1800
+# countdownTimer = 1800
 
 def end_read(signal,frame):
 
     global continue_reading
-    global countdownTimer
     print "Ctrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
     pass
 
 def show_time():
-    mylcd.lcd_display_string("Time remaining:", 1)
-    mylcd.lcd_display_string("30 minutes", 2)
+    mylcd.lcd_display_string("  WaitingTime  ", 1)
+    mylcd.lcd_display_string("  30 minutes  ", 2)
 
 signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
@@ -40,8 +39,7 @@ while continue_reading:
         mylcd.lcd_clear()
         mylcd.lcd_display_string("Reservation has", 1)
         mylcd.lcd_display_string("been made!", 2)
-        # countdownTimer = countdownTimer - 5
-        print "Card scanned"
+
         sleep(5)
 
     # countdownTimer = countdownTimer - 1
